72dbdac0d13f8768a23b554db332de14f2c1b8a4
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-08-09 19:09:43 +0800

    Rename and move files

--- aklish/proofreader/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def index(request, lang):
-    return render(request, "proofreader/index.html")
--- aklish/translate/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Language, Entry, Translation, Vote
-
-
-admin.site.register(Language)
-admin.site.register(Entry)
-admin.site.register(Translation)
-admin.site.register(Vote)
