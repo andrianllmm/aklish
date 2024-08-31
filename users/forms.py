624e0b97efd98d073da74e2f66ba7673882c921a
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2024-08-31 19:47:06 +0800

    Update login and register forms

--- users/forms.py
+++ users/forms.py
@@ -11,33 +11,35 @@ class RegisterForm(UserCreationForm):
 
         widgets = {
             "username": forms.TextInput(
-                attrs={"class": "form-control mb-2", "autofocus": "true"}
+                attrs={"class": "form-control mb-2", "autofocus": "true", "placeholder": "Username"}
             ),
             "email": forms.TextInput(
-                attrs={"required": "true", "class": "form-control mb-2"}
+                attrs={"required": "true", "class": "form-control mb-2", "placeholder": "Email"}
             ),
             "first_name": forms.TextInput(
-                attrs={"required": "true", "class": "form-control mb-2"}
+                attrs={"required": "true", "class": "form-control mb-2", "placeholder": "First name"}
             ),
             "last_name": forms.TextInput(
-                attrs={"required": "true", "class": "form-control mb-2"}
+                attrs={"required": "true", "class": "form-control mb-2", "placeholder": "Last name"}
             ),
         }
 
     password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control mb-2"})
+        widget=forms.PasswordInput(attrs={"class": "form-control mb-2", "placeholder": "Password"})
     )
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control mb-2"})
+        widget=forms.PasswordInput(attrs={"class": "form-control mb-2", "placeholder": "Confirm password"})
     )
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(
-            attrs={"class": "form-control mb-2", "autofocus": "true"}
+            attrs={"class": "form-control mb-2", "autofocus": "true", "placeholder": "Username"}
         )
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control mb-2"})
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control mb-2", "placeholder": "Password"}
+            )
     )
